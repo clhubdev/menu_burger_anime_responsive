8dcbc602cb1bbf441fe1703d0d51dc9c304bcc5b
Clément HUBERT <clhubertmail@gmail.com>
2022-05-25 17:29:15 +0200

    Version initiale du projet

---
+++ app.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () => {
+        navbar.classList.toggle('open-nav');
+    })
+}
+
+toggleMenu();
+
+console.log("Connexion réussie")
